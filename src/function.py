dd6e480f542887907decdb643231c66106e54512
Jillian Chan <jillian@thinkingmachin.es>
2024-08-13 09:26:45 +0000

    add workflow

---
+++ src/function.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import argparse
+
+
+def main(args):
+    print(f"num_a: {args.num_a}")
+    print(f"num_b: {args.num_b}")
+    print(f"sum: {args.num_a + args.num_b}")
+
+
+def parse_arguments():
+    desc = "Simple script"
+    parser = argparse.ArgumentParser(description=desc, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('--num_a', required=True, type=float, help='Number 1')
+    parser.add_argument('--num_b', required=True, type=float, help='Number 2')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    main(args)
